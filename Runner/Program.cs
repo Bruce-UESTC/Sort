588f4501baac24d776a37ca5b6d4c5103db4c4c7
Bruce-UESTC <106737173@qq.com>
2024-01-04 16:51:25 +0800

    添加项目文件。

---
+++ Runner/Program.cs
@@ -0,0 +1,57 @@
+﻿//#define BUBBLESORT
+#define MERGESORT
+
+using SortLib;
+
+#if BUBBLESORT
+
+Console.WriteLine("下面是冒泡排序测试：");
+int[] buff1 = RandomSequence(5);
+BubbleSortTest(buff1);
+
+#elif MERGESORT
+
+Console.WriteLine("下面是归并排序测试：");
+int[] buff2 = RandomSequence(10);
+MergeSortTest(buff2);
+
+#endif
+
+static int[] RandomSequence(int len)
+{
+    int[] buffer = new int[len];
+    Random rdm = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        buffer[i] = rdm.Next(100);
+    }
+
+    Console.WriteLine("{0}个数的随机序列:", len);
+
+    foreach (int i in buffer)
+    {
+        Console.WriteLine(i);
+    }
+
+    return buffer;
+}
+
+ static void BubbleSortTest(int[] buffer)
+{
+    Console.WriteLine("冒泡排序后的结果:");   
+    Sort.BubbleSort(buffer);
+
+    foreach (int i in buffer)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+static void MergeSortTest(int[] buffer)
+{     
+    Console.WriteLine("归并排序后的结果");
+    Sort.MergeSort(buffer);
+
+    foreach (int i in buffer)
+        Console.WriteLine(i);
+}
---
+++ SortLib/Sort.cs
@@ -0,0 +1,70 @@
+﻿namespace SortLib
+{
+    public class Sort
+    {
+        //**********  01-冒泡排序  **********//
+        public static void BubbleSort(int[] arr)
+        { 
+            int len = arr.Length;
+            int i, j, temp;
+            for (i = 0; i < len - 1; i++)
+                for (j = 0; j < len - 1 - i; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+
+        //**********  02-归并排序  **********//
+        public static void MergeSort(int[] arr) 
+        {
+            //int len = arr.Length;
+            if (arr.Length <= 1)
+            {
+                return;
+            }
+            else
+            {
+                int halflen = arr.Length / 2;
+                int[] tempa = new int[halflen];
+                int[] tempb = new int[arr.Length - halflen];
+
+                Array.Copy(arr, 0, tempa, 0, halflen);
+                Array.Copy(arr, halflen, tempb, 0, tempb.Length);
+
+                MergeSort(tempa);
+                MergeSort(tempb);
+
+                int a = 0;
+                int b = 0;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (a >= tempa.Length)
+                    {
+                        arr[i] = tempb[b++];
+                    }
+                    else if (b >= tempb.Length)
+                    {
+                        arr[i] = tempa[a++];
+                    }
+                    else
+                    {
+                        if (tempa[a] <= tempb[b])
+                        {
+                            arr[i] = tempa[a++];
+                        }
+                        else
+                        {
+                            arr[i] = tempb[b++];
+                        }
+                    }
+                }
+
+            }
+        }
+        
+    }
+}
